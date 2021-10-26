cbd37dfdf89aef4d04002b284c1eaf10944004d2
groomyjohny <groomyjohny@gmail.com>
2021-10-27 03:16:32 +0500

    Line drawing working

--- Sound Painter/src/main.cpp
+++ Sound Painter/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <SDL\SDL.h>
 
 #include "Helpers.h"
@@ -14,15 +15,51 @@ int main()
 	int windowX = SDL_WINDOWPOS_CENTERED;
 	int windowY = SDL_WINDOWPOS_CENTERED;
 
+	SDL_Window* wnd;
+	SDL_Renderer* rend;
+	SDL_CreateWindowAndRenderer(w, h, 0, &wnd, &rend);
+
+	std::vector<SDL_Point> points;
+
 	while (true)
 	{
-		SDL_Event event;
-		while (SDL_PollEvent(&event))
+		SDL_Event events;
+		while (SDL_PollEvent(&events))
 		{
 			//input processing goes here
+			if (events.key.keysym.scancode == SDL_SCANCODE_S)
+			{
+				//save routine
+			}
+			if (events.key.keysym.scancode == SDL_SCANCODE_C)
+			{
+				//clear routine
+				points.clear();
+			}
+
+			//if (events.type == SDL_MOUSEMOTION) this->onMouseMotion();
+			if (events.type == SDL_MOUSEBUTTONDOWN)  //mouse click
+			{
+				auto button = events.button.button;
+				int x, y;
+				SDL_GetMouseState(&x, &y);
+
+				if (button == SDL_BUTTON_LEFT)
+				{
+					points.emplace_back(SDL_Point{ x,y });
+				}
+			}
+			
+			if (events.key.keysym.scancode == SDL_SCANCODE_ESCAPE) exit(-1);
 		}
 
+		SDL_SetRenderDrawColor(rend, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		if (!points.empty()) SDL_RenderDrawLines(rend, &points.front(), points.size());
 		//frame rendering goes here
+		SDL_RenderPresent(rend);
+
+		SDL_SetRenderDrawColor(rend, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(rend);
 	}
 
 	std::cin.ignore(999999);
