8c8450a2a3a7f21e3973f48cf8a2de78d9204eab
groomyjohny <groomyjohny@gmail.com>
2021-11-05 05:30:09 +0500

    Changed sharps in HarmonicMixer::saveSoundToFile to unicode.

--- Sound Painter/src/HarmonicMixer.cpp
+++ Sound Painter/src/HarmonicMixer.cpp
@@ -1,4 +1,4 @@
-#include "HarmonicMixer.h"
+﻿#include "HarmonicMixer.h"
 #include <filesystem>
 HarmonicMixer::HarmonicMixer(int nHarmonics)
 {
@@ -24,16 +24,18 @@ void HarmonicMixer::saveSoundToFile(std::string fileName)
 {
 	size_t p0 = fileName.find_last_of('/');
 	size_t p1 = fileName.find('.');
-	
+
 	std::string baseName = fileName.substr(p0, p1 - p0);
 	std::string folderPath = fileName.substr(0, p1);
 	std::filesystem::create_directories(folderPath);
+	using fs = std::filesystem::path;
 
-	static std::string noteNames[] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A","A#","B" };
+	static std::wstring noteNames[] = { L"C", L"C♯", L"D", L"D♯", L"E", L"F", L"F♯", L"G", L"G♯", L"A",L"A♯", L"B" }; //unicode sharp signs for Polyphone
 	for (int i = 0; i < 128; ++i)
 	{
-		std::string noteName = noteNames[i % 12] + std::to_string(i / 12 - 1);
-		std::string subname = folderPath + baseName + std::string(" ") + noteName +".wav";
+		std::wstring noteName = noteNames[i % 12] + std::to_wstring(i / 12 - 1);
+		//std::wstring subname = folderPath + baseName + std::wstring(" ") + noteName + ".wav";
+		auto subname = std::filesystem::path(folderPath).concat(baseName).concat(" ").concat(noteName).concat(".wav");
 		setFundamentalFrequency(440 * pow(2, (i - 69) / 12.0));
 		Mixer::saveSoundToFile(subname);
 	}
@@ -60,7 +62,7 @@ void HarmonicMixer::setFundamentalFrequency(double frq)
 	this->fundamentalFrequency = frq;
 	for (int i = 0; i < nHarmonics; ++i)
 	{
-		events[i].setFrequency(frq*(i+1));
+		events[i].setFrequency(frq*(i + 1));
 	}
 }
 
